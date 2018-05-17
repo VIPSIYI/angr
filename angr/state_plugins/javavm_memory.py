c2d6cb7166315f9726e70cb10c6cde1344b01426
Sebastiano Mariani <mariani.sebastiano@gmail.com>
2018-05-17 16:44:29 -0700

    Fix stack copy over states

--- angr/state_plugins/javavm_memory.py
+++ angr/state_plugins/javavm_memory.py
@@ -64,7 +64,7 @@ class SimJavaVmMemory(SimMemory):
     def copy(self):
         return SimJavaVmMemory(
             memory_id=self.id,
-            stack=self._stack[::],
+            stack=[stack_frame.copy() for stack_frame in self._stack],
             heap=self.heap.copy(),
             vm_static_table=self.vm_static_table.copy()
         )
